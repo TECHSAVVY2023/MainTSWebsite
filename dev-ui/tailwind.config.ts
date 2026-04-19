e6888f19abcefa7e14a9dd7301cec51c0da47e03
mark <mark@gmail.com>
2026-04-19 22:10:03 +0800

    4/19/2026 10:09 PM Updates

--- dev-ui/tailwind.config.ts
+++ dev-ui/tailwind.config.ts
@@ -1,12 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+export default {
   content: [
-    "./app/components/**/*.{js,vue,ts}",
-    "./app/layouts/**/*.vue",
-    "./app/pages/**/*.vue",
-    "./app/plugins/**/*.{js,ts}",
-    "./app/app.vue",
-    "./app/error.vue",
+    './app/components/**/*.{js,vue,ts}',
+    './app/layouts/**/*.vue',
+    './app/pages/**/*.vue',
+    './app/plugins/**/*.{js,ts}',
+    './app/app.vue',
+    './app/error.vue',
   ],
   theme: {
     extend: {
@@ -57,4 +58,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+} satisfies Config
